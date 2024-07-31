9eb4de7a0afc728d9983459e0d46f8ac4451f75c
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-31 16:07:26 -0600

    .

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,20 @@
 const Sequelize = require('sequelize');
+const pg = require('pg');
 require('dotenv').config();
+
+const { Pool } = pg;
 
 let sequelize;
 if(process.env.DB_URL){
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -26,6 +37,10 @@ sequelize.authenticate()
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 
 module.exports = sequelize;
